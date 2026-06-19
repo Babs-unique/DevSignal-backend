e58b5d5cc0f95599562ac8406e7a5ed1f9ae3682
Babatunde Adegboye <babatundeadegboye2008@gmail.com>
2026-06-19 23:32:05 +0100

    feat(db): removed fallack for mongo db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import mongoose  from 'mongoose'
+import { env } from './env.js'
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        const conn = await mongoose.connect(env.MONGO_URI as string);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     }catch(e){
