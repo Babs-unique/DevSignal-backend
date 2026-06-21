1ec169385a9ac7a1426aae9f63c293e9ea8135b2
Babatunde Adegboye <babatundeadegboye2008@gmail.com>
2026-06-21 09:30:58 +0100

    feat(debug): debugging db conn issue

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,7 @@ import { env } from './env.js'
 
 export const connectDB = async () => {
     try {
+        console.log(`${env.MONGO_URI} is connected`)
         const conn = await mongoose.connect(env.MONGO_URI as string);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
